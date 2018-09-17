e2ed5ffe1384cdbfa98af1aa81a6e22a14af8219
alexsinfarosa <as898@cornell.edu>
2018-09-17 18:07:59 +0200

    updates

--- src/stores/AppStore.js
+++ src/stores/AppStore.js
@@ -1,29 +1,7 @@
-import { decorate, when, observable, action } from "mobx";
 import ParamsStore from "./ParamsStore";
 
 export default class AppStore {
-  stations;
-  paramsStore;
-  setStations = d => (this.stations = d);
   constructor() {
-    when(
-      () => !this.stations,
-      () => {
-        fetch("stationList2.json")
-          .then(res => res.json())
-          .then(stns => {
-            this.setStations(stns);
-          })
-          .catch(err => {
-            console.log("Failed to load stations", err);
-          });
-      }
-    );
     this.paramsStore = new ParamsStore();
   }
 }
-
-decorate(AppStore, {
-  stations: observable,
-  setStations: action
-});
